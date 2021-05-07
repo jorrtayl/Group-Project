a50bff06221c347f41619119433757f5f4b25bbd
Jordan Taylor <jorrtayl@ut.utm.edu>
2021-05-07 06:35:35 -0500

    Updated Working Sphere with Gravity
    Sphere can now move and is affected by gravity. Weird sliding bug but did what I intended to do.

--- GroupProject/Assets/Code/SphereMovement.cs
+++ GroupProject/Assets/Code/SphereMovement.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SphereMovement : MonoBehaviour
@@ -11,6 +13,12 @@ public class SphereMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float speed = .1f;
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
+
+        transform.position += moveDirection * speed;
     }
 }
